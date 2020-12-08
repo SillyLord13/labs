7a92d30088e7ae2dc89e22b2180650d50d2a059e
SillyLord13 <54590407+SillyLord13@users.noreply.github.com>
2020-12-08 14:28:41 -0600

    Add files via upload

---
+++ wouldyourather/src/App.vue
@@ -0,0 +1,85 @@
+<template>
+  <div id="app">
+
+    <h1>Would you rather...</h1>
+
+    <would-you-rather v-for="wyr in questions"
+                      v-bind:id="wyr.id"
+                      v-bind:question="wyr.question"
+                      v-bind:answer1="wyr.answer1"
+                      v-bind:answer2="wyr.answer2"
+                      v-on:answer-change="answerChange">
+    </would-you-rather>
+
+    <h1>So you would rather...</h1>
+    <ul>
+      <li class="choices" v-for="answer in answers">
+        <h3>{{answer.userChoice}}</h3>
+      </li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+import WouldYouRather from './components/WouldYouRather.vue'
+
+export default {
+  name: 'App',
+  components: {
+    WouldYouRather
+  },
+  data() {
+    return{
+      questions: [
+        {
+          id: 1,
+          question: 'Would you rather be an incredibly fast runner or an incredibly fast swimmer?',
+          answer1: 'Be a fast Swimmer',
+          answer2: 'Be a fast Runner'
+        },
+        {
+          id: 2,
+          question: 'Would you rather watch the original Star Wars trilogy but it slows down everytime someone says the force ' +
+              'or watch the Lord of the Rings trilogy but it speeds up everytime Sam says potato?',
+          answer1: 'Watch slowed down Star Wars',
+          answer2: 'Watch sped up Lord of the Rings'
+        },
+        {
+          id: 3,
+          question:'Would you rather have the ability to speak to animals or the ability to read people\'s minds?',
+          answer1:' Be able to speak to Animals',
+          answer2: 'Be able to read Minds'
+        }
+      ],
+      answers:[],
+
+    }
+  },
+  methods: {
+    // add answer to display list
+    answerChange(choice){
+      this.answers.push(choice)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  border: solid black 3px ;
+  border-radius: 15px;
+  padding: 5px;
+
+}
+body{
+  background: lightskyblue;
+}
+
+</style>
---
+++ wouldyourather/src/components/WouldYouRather.vue
@@ -0,0 +1,57 @@
+<!--components dont store values, but they store types. this way a companent can be re-used in-->
+<!--different places for an app.-->
+<template>
+  <div class="wyr">
+
+      <h2>Please make your choice </h2>
+
+      <h3>{{question}}</h3>
+    <input type="radio" v-model="choice"  v-bind:value="answer1" v-on:change="choiceMade">
+    <label>{{answer1}}</label>
+    <input type="radio" v-model="choice" v-bind:value="answer2" v-on:change="choiceMade">
+    <label>{{answer2}}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WouldYouRather',
+  // data provided by parent and sent to child component
+  //NEVER MODIFY A PROP
+  props: {
+    //stores type rather than value
+    question: String,
+    answer1: String,
+    answer2: String,
+    id: Number,
+    answers: Array,
+  },
+  data(){
+    return{
+      choice: ''
+    }
+  },
+  methods: {
+    choiceMade (){
+      //$emit sends an event or a message to the parent
+      this.$emit('answer-change',this.choice)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+.wyr{
+
+  border-radius: 25px;
+  padding-bottom: 15px;
+  box-shadow: 2px 2px 2px 1px;
+  background: white;
+  padding-bottom: 5px;
+}
+
+</style>
